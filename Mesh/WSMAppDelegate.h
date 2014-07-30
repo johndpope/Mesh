20fce0ada4980f2032ad88c4d16bb6fe5e8a1430
Cristian Monterroza <cmonterroza@wrkstrm.me>
2014-07-30 16:30:34 -0700

    Initial Commit

---
+++ Mesh/WSMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSMAppDelegate.h
+//  Mesh
+//
+//  Created by Cristian Monterroza on 7/30/14.
+//  Copyright (c) 2014 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mesh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mesh
+//
+//  Created by Cristian Monterroza on 7/30/14.
+//  Copyright (c) 2014 wrkstrm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSMAppDelegate class]));
+    }
+}
